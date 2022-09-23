9362c0fbc6b0ad3e35e8c79e756757de05c46e27
unknown <rodarg87@gmail.com>
2022-09-23 12:11:12 -0300

    validando forms

--- Resto/src/components/InicioSesion.vue
+++ Resto/src/components/InicioSesion.vue
@@ -1,74 +1,74 @@
-<script setup>
+
+<script >
 import Encabezado from "./Encabezado.vue";
 import Footer from "./Footer.vue";
 
+export default {
+  data() {
+    return {
+      Usuario: {user:"", pass:""},
+      errorUser: false,
+      errorPass: false,
+    }
+  },
+  methods: {
+    validar(e) {
+        this.errorUser = this.Usuario.user.length < 3;
+        this.errorPass = this.Usuario.pass.length < 3;
+        if (!this.errorUser && !this.errorPass) {
+        return true;
+      }
+        e.preventDefault();
+      }
+      
+
+    }
+  }
 </script>
     
-    <template>
+<template>
+
   <body class="main">
     <div id="main">
       <div id="inner">
         <Encabezado />
-        <div
-          class="
+        <div class="
             d-flex
             flex-column
             min-vh-100
             justify-content-center
             align-items-center
-          "
-        >
+          ">
           <div class=" card p-4 text-light bg-secondary mb-5 rounded-lg ancho">
             <div class="card-header">
               <h3>Iniciar sesión</h3>
             </div>
             <div class="card-body w-100 mt-3">
-              <form name="login" action="" method="post">
+              <form @submit="validar" method="post">
                 <div class="input-group form-group mt-3">
                   <div class="bg-dark rounded-start d-flex align-items-center">
                     <span class="m-3 "><i class="bi bi-person mt-3"></i></span>
                   </div>
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Usuario"
-                    name="username"
-                  />
+                  <input type="text" class="form-control" placeholder="Usuario" v-model="Usuario.user"/>
                 </div>
+                <div v-if="errorUser" class="text-danger">Usuario invalido</div>
                 <div class="input-group form-group mt-3">
                   <div class="bg-dark rounded-start d-flex align-items-center">
                     <span class="m-3 "><i class="bi bi-lock"></i></span>
                   </div>
-                  <input
-                    type="password"
-                    class="form-control"
-                    placeholder="Contraseña"
-                    name="password"
-                  />
+                  <input type="password" class="form-control" placeholder="Contraseña" v-model="Usuario.pass" />
                 </div>
-
+                <div v-if="errorPass" class="text-danger">password invalido</div>
                 <div class="form-group mt-5">
-                  <input
-                    type="submit"
-                    value="Acceder"
-                    class="btn btn-dark float-end text-white w-100"
-                    name="login-btn"
-                  />
+                  <input type="submit" value="Acceder" class="btn btn-dark float-end text-white w-100"/>
                 </div>
               </form>
             </div>
             <div class="card-footer">
               <div class="d-flex justify-content-center container">
-                <a
-                  href="registro.html"
-                  class="col-md-3"
-                  name="register-btn"
-                  >Registrarse</a
-                >
+                <a href="registro.html" class="col-md-3" name="register-btn">Registrarse</a>
                 <div class="col-md-1"></div>
-                <a class="col-md-5" name="olvido"
-                  >¿Olvidaste tu contraseña?</a
-                >
+                <a class="col-md-5" name="olvido">¿Olvidaste tu contraseña?</a>
               </div>
             </div>
           </div>
--- Resto/src/components/Registro.vue
+++ Resto/src/components/Registro.vue
@@ -3,77 +3,44 @@ import Encabezado from "./Encabezado.vue";
 import Footer from "./Footer.vue";
 </script>
         
-        <template>
+<template>
+
   <body class="main">
     <div id="main">
       <div id="inner">
         <Encabezado />
-        <div
-          class="
+        <div class="
             d-flex
             flex-column
             min-vh-100
             justify-content-center
             align-items-center
-          "
-        >
+          ">
           <div class="card p-4 text-light bg-secondary mb-5 rounded-lg ancho mt-5">
             <div class="card-header">
               <h3>CREAR TU CUENTA</h3>
             </div>
             <div class="card-body w-100 mt-3">
-              <form name="registro" @submit="validate()">
+              <form name="registro" @submit="validar" method="post">
                 <div class="input-group form-group mt-3">
                   <label class="w-100" for="username">User name</label>
-                  <input
-                    type="text"
-                    class="form-control w-100 mb-3"
-                    placeholder="Usuario"
-                    v-model="User"
-                  />
-                  <span id="name" class="invisible" style="color: red"></span>
-                  <label class="w-100" for="pass"
-                    >Dirección de correo electrónico</label
-                  >
-                  <input
-                    type="email"
-                    class="form-control w-100 mb-3"
-                    placeholder="email@email.com"
-                    name="email"
-                  />
-                  <label class="w-100" for="confirmPass"
-                    >Confirma tu dirección</label
-                  >
-                  <input
-                    type="email"
-                    class="form-control w-100 mb-3"
-                    placeholder="email@email.com"
-                    name="confirmEmail"
-                  />
+                  <input type="text" class="form-control w-100 mb-3" placeholder="Usuario" v-model="Usuario.user" />
+                  <span id="name" class="text-danger" v-if="errorUser"> Usuario no valido</span>
+                  <label class="w-100" for="pass">Dirección de correo electrónico</label>
+                  <input type="email" class="form-control w-100 mb-3" placeholder="email@email.com" />
+                  <span id="name" class="text-danger" v-if="errorEmail">Mail invalido</span>
+                  <label class="w-100" for="confirmPass">Confirma tu dirección</label>
+                  <input type="email" class="form-control w-100 mb-3" placeholder="email@email.com" />
+                  <span id="name" class="text-danger" v-if="errorConfEmail"></span>
                   <label class="w-100" for="pass">Password</label>
-                  <input
-                    type="password"
-                    class="form-control w-100 mb-3"
-                    placeholder="Contraseña"
-                    name="pass"
-                  />
-                  <label for="confirmPass" class="w-100"
-                    >Confirma tu Password</label
-                  >
-                  <input
-                    type="password"
-                    class="form-control w-100 mb-3"
-                    placeholder="Contraseña"
-                    name="confirmPass"
-                  />
+                  <input type="password" class="form-control w-100 mb-3" placeholder="Contraseña" />
+                  <span id="name" class="text-danger"></span>
+                  <label for="confirmPass" class="w-100">Confirma tu Password</label>
+                  <input type="password" class="form-control w-100 mb-3" placeholder="Contraseña" />
+                  <span id="name" class="text-danger"></span>
                 </div>
                 <div class="form-group mt-5">
-                  <input
-                    type="submit"
-                    value="Registrarse"
-                    class="btn btn-dark float-end text-white w-100"
-                    name="login-btn"
-                  />
+                  <input type="submit" value="Registrarse" class="btn btn-dark float-end text-white w-100" />
                 </div>
               </form>
             </div>
@@ -87,11 +54,25 @@ import Footer from "./Footer.vue";
 <script>
 export default {
   data: () => ({
-    errorUser:null,
-    errorEmail:null,
-    errorConfEmail:null,
+    Usuario: {},
+    reg: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    errorUser: false,
+    errorEmail: false,
+    errorConfEmail: false,
+    errorPass: false,
+    errorConfPass: false
   }),
   methods: {
+    validar(e) {
+      this.errorUser = this.Usuario.user ==undefined || this.Usuario.user.length < 3;
+      
+      this.errorEmail = this.reg.test(this.Usuario.email);
+      if (!this.errorUser && !this.errorEmail && !this.errorConfEmail && !this.errorPass && !this.errorConfPass) {
+        return true
+        
+      }
+      e.preventDefault();
+    }
   },
 };
 </script>
