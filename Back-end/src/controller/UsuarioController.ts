bd7d85a58259c17f64a52de940bc8277459e41ac
rodarg877 <rodarg87@gmail.com>
2022-10-22 17:32:20 -0300

    modelando el back

--- Back-end/src/controller/UsuarioController.ts
+++ Back-end/src/controller/UsuarioController.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express'
+import Usuario from '../models/Usuario.js';
 import UsuarioService from '../service/UsuarioService.js';
 
 export default class UsuarioController {
@@ -8,5 +9,14 @@ export default class UsuarioController {
             res.json(await UsuarioService.obtenerUsuarios(nick))
 
         }
-    
+        public static async postUsuarios(req: Request, res: Response) {
+            const user =req.body as unknown as Usuario
+            res.json(await UsuarioService.loginUsuario(user))
+
+        }
+        public static async postUsuario(req: Request, res: Response) {
+            const user =req.body as unknown as Usuario
+            res.json(await UsuarioService.agregarUsuario(user))
+
+        }
 }
